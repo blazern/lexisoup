ecedfc9124d73ac9c37039527474f5c9f8a9283e
blazern <danil.garmanov@protonmail.com>
2026-03-02 08:59:48 +0100

    copy words by clicks, open context menu by long click

--- lexisoup-kmp/feature/search-results/src/commonMain/kotlin/blazern/lexisoup/feature/search_results/ui/ContextMenuTrigger.kt
+++ lexisoup-kmp/feature/search-results/src/commonMain/kotlin/blazern/lexisoup/feature/search_results/ui/ContextMenuTrigger.kt
@@ -1,6 +1,6 @@
 package blazern.lexisoup.feature.search_results.ui
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -15,14 +15,20 @@ import androidx.compose.ui.Modifier
 @Composable
 internal fun ContextMenuTrigger(
     actions: List<ContextAction>,
+    clickActionIndex: Int,
     content: @Composable () -> Unit,
 ) {
     var menuExpanded by remember { mutableStateOf(false) }
 
     Box(
-        modifier = Modifier.clickable {
-            menuExpanded = true
-        }
+        modifier = Modifier.combinedClickable(
+            onClick = {
+                actions[clickActionIndex].onClick()
+            },
+            onLongClick = {
+                menuExpanded = true
+            }
+        )
     ) {
         content()
         DropdownMenu(
